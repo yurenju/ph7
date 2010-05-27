045942773831bc907c4dc1f8b6e398534d80d4e6
Yuren Ju <yurenju@gmail.com>
2010-05-27 22:54:01 +0800

    add shot button

--- src/org/ph7/NewReportActivity.java
+++ src/org/ph7/NewReportActivity.java
@@ -1,11 +1,25 @@
 package org.ph7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 
 public class NewReportActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newreport);
+        
+        final Button button = (Button)findViewById(R.id.ButtonShot);
+        button.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View arg0) {
+				Intent intent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivity(intent);
+				
+			}
+		});
     }
 }
